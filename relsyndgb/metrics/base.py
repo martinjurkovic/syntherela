f9f346c5cffcaa23bcfac89cea16f45faf0bf7bd
ValterH <valter.hudovernik@gmail.com>
2024-05-29 16:39:22 +0200

    remove TODO

--- relsyndgb/metrics/base.py
+++ relsyndgb/metrics/base.py
@@ -222,8 +222,8 @@ class DetectionBaseMetric(BaseMetric):
     def bootstrap_sample(real_data, random_state=None, metadata=None):
         return real_data.sample(frac=1, replace=True, random_state=random_state)
     
-    # TODO: raise bootstrap repetitions (m) to 100 or 1000
-    def baseline(self, real_data, metadata, m=10, **kwargs):
+    
+    def baseline(self, real_data, metadata, m=1000, **kwargs):
         bootstrap_scores = []
         for i in range(m):
             sample1 = self.bootstrap_sample(real_data, random_state=i, metadata=metadata)
