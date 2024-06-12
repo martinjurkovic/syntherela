644722de3e4ccae6dcef9cf951744214b9eab81f
ValterH <valter.hudovernik@gmail.com>
2024-06-12 11:06:25 +0200

    add logging to figure/table generation

--- experiments/reproducibility/generate_figures.sh
+++ experiments/reproducibility/generate_figures.sh
@@ -1,5 +1,11 @@
+echo "Drawing figure 2"
 python experiments/figures/figure2.py
+echo "Drawing figure 3"
 python experiments/figures/figure3.py
+echo "Drawing figure 4"
 python experiments/figures/figure4.py
+echo "Drawing figure 5"
 python experiments/figures/figure5.py
+echo "Drawing figure 6"
 python experiments/figures/figure6.py
+echo "Figures can be found in results/figures"
--- experiments/reproducibility/generate_tables.sh
+++ experiments/reproducibility/generate_tables.sh
@@ -1,2 +1,5 @@
+echo "Writing fidelity tables (1, 8, 9)"
 python experiments/tables/fidelity.py
+echo "Writing utility tables (2, 6, 7)"
 python experiments/tables/utility.py
+echo "Tables can be found in results/tables"
