357eeb4c58e06682562574c9e0f0f44327442ee4
ValterH <valter.hudovernik@gmail.com>
2024-11-28 14:43:13 +0100

    add reproducibility script

---
+++ experiments/reproducibility/generation/generate_mostlyai.sh
@@ -0,0 +1,22 @@
+DATASETS=(
+    airbnb-simplified_subsampled
+    Biodegradability_v1
+    CORA_v1
+    imdb_MovieLens_v1
+    rossmann_subsampled
+    walmart_subsampled
+)
+
+RUN_IDS=(
+    1 
+    2 
+    3
+)
+
+for DATASET in ${DATASETS[@]}
+do
+    for RUN_ID in ${RUN_IDS[@]}
+    do
+        python experiments/generation/mostlyai/generate_mostlyai.py --dataset-name $DATASET --run-id $RUN_ID --api-key $1 
+    done
+done
