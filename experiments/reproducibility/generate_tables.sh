7c086e1607a1f56e76b1a015f3afa5b65f290334
ValterH <valter.hudovernik@gmail.com>
2025-05-20 19:31:34 +0200

    update table reproducibility script

--- experiments/reproducibility/generate_tables.sh
+++ experiments/reproducibility/generate_tables.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 mkdir -p results/tables
-echo "Writing fidelity tables."
+echo "Writing fidelity tables (2, 3, 7)."
 python experiments/tables/fidelity.py
-echo "Writing utility table."
+echo "Writing utility table (4)."
 python experiments/tables/gnn_utility.py
-echo "Writing rdl utility table."
-python experiments/tables/rdl_utility.py
-echo "Writing privacy table."
+echo "Writing privacy table (5)."
 python experiments/tables/privacy.py
+echo "Writing fidelity - utility correlation table (8)"
+python experiments/tables/fidelity_utility_correlation.py
 echo "Tables can be found in results/tables"
