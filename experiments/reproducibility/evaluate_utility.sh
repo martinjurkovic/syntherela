af3c5fc1d6518685bac52716f6a5c22213d28143
Martin Jurkovic <martin@viaduct.ai>
2025-05-20 20:40:16 +0200

    Update utility reproducibility script

--- experiments/reproducibility/evaluate_utility.sh
+++ experiments/reproducibility/evaluate_utility.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 python experiments/evaluation/rdl_utility/run_utility_benchmark.py
-python experiments/evaluation/rdl_utility/run_singletable_benchmark.py --singletable_model singletable
-python experiments/evaluation/rdl_utility/run_singletable_benchmark.py --singletable_model singletable_dfs
