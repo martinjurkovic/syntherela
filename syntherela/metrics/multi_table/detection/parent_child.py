7d4a748049a79417436c96341e5c68a783f173c0
ValterH <valter.hudovernik@gmail.com>
2025-07-31 17:21:34 -0700

    remove DenormalizationDetection

--- syntherela/metrics/multi_table/detection/parent_child.py
+++ syntherela/metrics/multi_table/detection/parent_child.py
@@ -113,20 +113,80 @@ class ParentChildDetection(DetectionBaseMetric):
             - y: The labels for the real and synthetic data.
 
         """
-        real_data_pair = {
-            parent_table: real_data[parent_table],
-            child_table: real_data[child_table],
-        }
-        synthetic_data_pair = {
-            parent_table: synthetic_data[parent_table],
-            child_table: synthetic_data[child_table],
-        }
-        return super().prepare_data(
-            real_data_pair,
-            synthetic_data_pair,
-            pair_metadata,
-            parent_table,
+        real_data_unique, synthetic_data_unique, metadata_unique = (
+            make_column_names_unique(
+                {
+                    parent_table: real_data[parent_table],
+                    child_table: real_data[child_table],
+                },
+                {
+                    parent_table: synthetic_data[parent_table],
+                    child_table: synthetic_data[child_table],
+                },
+                deepcopy(metadata),
+                validate=False,
+            )
         )
+        denormalized_real_data = denormalize_tables(real_data_unique, metadata_unique)
+        denormalized_synthetic_data = denormalize_tables(
+            synthetic_data_unique, metadata_unique
+        )
+        for table in metadata_unique.get_tables():
+            table_metadata = metadata_unique.tables[table].to_dict()
+            if table == parent_table:
+                parent_id = metadata_unique.get_primary_key(table)
+                real_ids = denormalized_real_data[parent_id]
+                synthetic_ids = denormalized_synthetic_data[parent_id]
+            denormalized_real_data = drop_ids(denormalized_real_data, table_metadata)
+            denormalized_synthetic_data = drop_ids(
+                denormalized_synthetic_data, table_metadata
+            )
+
+            n = min(
+                denormalized_real_data.shape[0], denormalized_synthetic_data.shape[0]
+            )
+            idx_real = np.random.choice(denormalized_real_data.index, n, replace=False)
+            idx_synthetic = np.random.choice(
+                denormalized_synthetic_data.index, n, replace=False
+            )
+            real_data = denormalized_real_data.loc[idx_real].reset_index(drop=True)
+            synthetic_data = denormalized_synthetic_data.loc[idx_synthetic].reset_index(
+                drop=True
+            )
+            real_ids = real_ids.loc[idx_real].reset_index(drop=True)
+            synthetic_ids = synthetic_ids.loc[idx_synthetic].reset_index(drop=True)
+
+            ht = CustomHyperTransformer()
+            combined_data = pd.concat([real_data, synthetic_data])
+            ht.fit(combined_data)
+            transformed_real_data = ht.transform(real_data.copy())
+            transformed_synthetic_data = ht.transform(synthetic_data.copy())
+
+            unique_real_ids = np.unique(real_ids)
+            unique_synthetic_ids = np.unique(synthetic_ids)
+            ids_train_real = np.random.choice(
+                unique_real_ids, len(unique_real_ids) // 2, replace=False
+            )
+            ids_train_synthetic = np.random.choice(
+                unique_synthetic_ids, len(unique_synthetic_ids) // 2, replace=False
+            )
+            mask_train_real = real_ids.isin(ids_train_real).values
+            mask_train_synthetic = synthetic_ids.isin(ids_train_synthetic).values
+
+            X_train_real = transformed_real_data[mask_train_real]
+            X_train_synthetic = transformed_synthetic_data[mask_train_synthetic]
+            X_test_real = transformed_real_data[~mask_train_real]
+            X_test_synthetic = transformed_synthetic_data[~mask_train_synthetic]
+
+            X_train = pd.concat([X_train_real, X_train_synthetic])
+            X_test = pd.concat([X_test_real, X_test_synthetic])
+            y_train = np.hstack(
+                [np.ones(len(X_train_real)), np.zeros(len(X_train_synthetic))]
+            )
+            y_test = np.hstack(
+                [np.ones(len(X_test_real)), np.zeros(len(X_test_synthetic))]
+            )
+        return X_train, X_test, y_train, y_test
 
     def run(self, real_data: dict, synthetic_data: dict, metadata: Metadata, **kwargs):
         """Run the parent-child detection metric on all parent-child relationships.
